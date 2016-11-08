4229b4c4aca17535b88337afb931367b10cdc306
shijin <shi.jin@outlook.com>
2016-11-08 23:56:01 +0800

    1. custom.el, 从 init.el 独立出来 2. init.el, 根据子龙闪人的配置，删除一些不常用的 package

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # 概述
-Spacemacs个人初始化文件，包含日常基本功能，包括*flycheck*结合*modelsim*检查*verilog*语法。
+Spacemacs个人初始化文件，包含日常基本功能，包括**flycheck + modelsim**检查**verilog**语法。
 
 # TODO
 + 逐渐规范该初始化文件，根据不同作用将设置分配至不同的Elisp文件
 + 开始使用org-mode，提升工作效率
+
+# 使用环境
++ spacemacs **0.200.5**
++ emacs-w64 **25.1**
--- init.el
+++ init.el
@@ -58,7 +58,12 @@ values."
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
-   dotspacemacs-excluded-packages '(YASnippet)
+   ;; 1. holy-mode R
+   dotspacemacs-excluded-packages '(
+                                    magit-gh-pulls magit-gitflow org-projectile
+                                    org-bullets smooth-scrolling org-repo-todo org-download
+                                    org-timer org-present google-translate fancy-battery
+                                    git-gutter git-gutter-fringe)
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
    ;; `used-only' installs only explicitly used packages and uninstall any
@@ -358,23 +363,9 @@ Please See Modelsim Manual, Command 'vlog'."
 
   (add-hook 'verilog-mode-hook (lambda ()
                                  (setq flycheck-checker 'verilog-modelsim)
-                                 (flycheck-mode 1)))
+                                 (flycheck-mode -1)
+                                 (yas-minor-mode -1)))
   )
 
-;; Do not write anything past this comment. This is where Emacs will
-;; auto-generate custom variable definitions.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(flycheck-modelsim-work-path (quote ("../../tcl/work")))
- '(package-selected-packages
-   (quote
-    (org-projectile pcache org-present org org-pomodoro alert log4e gntp org-download mwim mmm-mode markdown-toc markdown-mode htmlize helm-company helm-c-yasnippet gnuplot gh-md flycheck-pos-tip pos-tip flycheck company-statistics company auto-yasnippet yasnippet ac-ispell auto-complete ws-butler window-numbering which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide ido-vertical-mode hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed dash aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async quelpa package-build spacemacs-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
+(load custom-file 'no-error 'no-message)
